b2a521e10a5f6f4addb58197ea154c349e9617bc
Gianluigi Zanetti <zag@crs4.it>
2018-10-22 10:10:54 +0200

    Added a basic image warp function.

--- tdm/radar/utils.py
+++ tdm/radar/utils.py
@@ -70,3 +70,33 @@ def estimate_rainfall(signal, mask):
     "This is specific to XXX radar signal"
     Z = 10**(0.1*(0.39216 * signal - 8.6))
     return (Z/300)**(1/1.5) * mask
+
+
+
+# From https://gis.stackexchange.com/questions/139906/replicating-result-of-gdalwarp-using-gdal-python-bindings
+def get_wkt(srs_code):
+    # checks? we need no stinking checks!
+    geo, code = srs_code.split(':')
+    srs = osr.SpatialReference()
+    # force geo to EPSG for the time being
+    srs.ImportFromEPSG(int(code))
+    return srs.ExportToWkt()
+
+
+def warp(in_path, out_path, t_srs, s_srs=None, error_threshold=None):
+    src_ds = gdal.Open(in_path)
+    dst_wkt = get_wkt(t_srs)
+    dst_srs = osr.SpatialReference()
+    src_wkt = None if s_srs is None else get_wkt(s_srs)
+    error_threshold = error_threshold if error_threshold is not None else 0.125
+    resampling = gdal.GRA_NearestNeighbour
+    # Call AutoCreateWarpedVRT() to fetch default values for target raster
+    # dimensions and geotransform
+    tmp_ds = gdal.AutoCreateWarpedVRT(src_ds,
+                                      src_wkt,
+                                      dst_wkt,
+                                      resampling,
+                                      error_threshold)
+    # Create the final warped raster
+    dst_ds = gdal.GetDriverByName('GTiff').CreateCopy(out_path, tmp_ds)
+
