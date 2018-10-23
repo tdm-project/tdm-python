67dcb0bde1083b3d429591b5382ef7a62dc6dd40
simleo <simleo@crs4.it>
2018-10-23 18:10:36 +0200

    install radar subpackage

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='tdm modules and tools',
     author='ALfred E. Neumann',
     author_email='aen@gmail.com',
-    packages=['tdm', 'tdm.gfs', 'tdm.gfs.noaa', 'tdm.wrf'],
+    packages=['tdm', 'tdm.gfs', 'tdm.gfs.noaa', 'tdm.radar', 'tdm.wrf'],
     scripts=['bin/gfs_fetch', 'bin/wrf_configurator', 'bin/link_grib'],
 )
---
+++ test/gfs/test_noaa_fetcher.py
@@ -0,0 +1,5 @@
+from tdm.gfs.noaa import noaa_fetcher
+
+# TBD
+if __name__ == "__main__":
+    print(noaa_fetcher)
