3d3b03adabf11cccf3fba5e9b0ab3e3317b1be84
Gianluigi Zanetti <zag@crs4.it>
2018-12-16 14:18:51 +0100

    Update grib2cf runner.

--- tools/runners/grib2cf.sh
+++ tools/runners/grib2cf.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
-in_dir="/data/simulations/grib2"
-out_dir="/data/simulations/netcdf"
+# bash grib2cf.sh in_dir out_dir
+
+in_dir=${1:=/data/simulations/grib2}
+out_dir=${2:/data/simulations/netcdf}
 
 wd=$(mktemp -d)
 echo "work dir: ${wd}"
@@ -14,7 +16,7 @@ rd="${wd}"/results
 find "${in_dir}" -mindepth 1 -maxdepth 1 -type d > "${lst}"
 parallel -a "${lst}" -j32 --progress --joblog "${log}" --results "${rd}" \
   docker run --rm \
-    -v "${in_dir}":"${in_dir}":ro -v "${out_dir}":"${out_dir}" \
-    crs4/tdm-tools tdm_grib2cf -i {} -o "${out_dir}"\;
+    -v "${in_dir}":/tmp/in_dir:ro -v "${out_dir}":/tmp/out_dir \
+    crs4/tdm-tools tdm_grib2cf -i {} -o /tmp/out_dir\;
 
 echo "work dir was: ${wd}"
