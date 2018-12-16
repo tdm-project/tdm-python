9ea91e0c1327100266c94ca503719dd17a0968a9
Gianluigi Zanetti <zag@crs4.it>
2018-12-16 15:57:02 +0100

    bug fix.

--- tools/runners/grib2cf.sh
+++ tools/runners/grib2cf.sh
@@ -16,7 +16,7 @@ rd="${wd}"/results
 find "${in_dir}" -mindepth 1 -maxdepth 1 -type d > "${lst}"
 parallel -a "${lst}" -j32 --progress --joblog "${log}" --results "${rd}" \
   docker run --rm \
-    -v "${in_dir}":/tmp/in_dir:ro -v "${out_dir}":/tmp/out_dir \
-    crs4/tdm-tools tdm_grib2cf -i {} -o /tmp/out_dir\;
+    -v "${in_dir}":"${in_dir}":ro -v "${out_dir}":"${out_dir}" \
+    crs4/tdm-tools tdm_grib2cf -i {} -o "${out_dir}"\;
 
 echo "work dir was: ${wd}"
