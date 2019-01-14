98a05f1ad7f27807db1a104a46103e675cf5ea08
simleo <simleo@crs4.it>
2019-01-14 17:37:35 +0100

    updated setup metadata

--- setup.py
+++ setup.py
@@ -3,9 +3,24 @@ from distutils.core import setup
 setup(
     name='tdm',
     version='0.0',
-    description='tdm modules and tools',
-    author='ALfred E. Neumann',
-    author_email='aen@gmail.com',
+    url='https://github.com/tdm-project/tdm-tools',
+    description='TDM Tools - http://www.tdm-project.it/en/',
+    author=', '.join((
+        'Simone Leo',
+        'Gianluigi Zanetti',
+    )),
+    author_email=', '.join((
+        '<simone.leo@crs4.it>',
+        '<gianluigi.zanetti@crs4.it>',
+    )),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Intended Audience :: Science/Research',
+    ],
     packages=['tdm', 'tdm.gfs', 'tdm.gfs.noaa', 'tdm.radar', 'tdm.wrf'],
     scripts=[
         'bin/tdm_gfs_fetch',
