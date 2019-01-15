f47dd2691e74efd0dbccae3cacb06dd4a7dc04d9
simleo <simleo@crs4.it>
2019-01-15 18:18:04 +0100

    PEP8 fix

--- tdm/wrf/configuration_checker.py
+++ tdm/wrf/configuration_checker.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class configuration_checker(object):
     def __init__(self, configuration):
         self.configuration = configuration
