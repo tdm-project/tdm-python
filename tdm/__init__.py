5c90adbbda654f32fd72ec9d685da4e72895e00c
simleo <simleo@crs4.it>
2019-01-16 17:42:38 +0100

    single source version in Python module

--- tdm/__init__.py
+++ tdm/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-try:
-    from tdm.version import VERSION as __version__
-except ImportError:
-    __version__ = None
+from tdm.version import VERSION
+
+__version__ = VERSION
